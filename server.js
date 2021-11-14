441c6461e12365636daf57453615fcce408d1ded
Dante B1ase <Dante B1ase@DESKTOP-M018U9H>
2021-11-14 17:57:37 -0500

    test

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-app.use(cors())
+// app.use(cors())
+app.set('view engine', 'ejs')
 app.use(express.static(__dirname))
 
 app.get('/', function(req, res) {
     res.sendFile('index.html', { root: __dirname })
 });
 
-app.listen(process.env.port || 3000)
+app.listen(process.env.port || 3000, () => {})
 
